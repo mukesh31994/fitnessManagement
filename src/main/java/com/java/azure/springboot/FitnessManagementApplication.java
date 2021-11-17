2e3b4062be1cf2d5ba32a1fe4acbb182972311f0
unknown <mukeshsharma31994@gmail.com>
2021-11-17 19:36:18 +0000

    first commit

--- src/main/java/com/java/azure/springboot/FitnessManagementApplication.java
+++ src/main/java/com/java/azure/springboot/FitnessManagementApplication.java
@@ -15,12 +15,12 @@ public class FitnessManagementApplication {
 	
 	@GetMapping("welcome")
 	public String getMessage() {
-		return "Welcome to Azure Web App for Containers ";
+		return "Welcome to Azure Web App Commit 2";
 	}
 	
 	@GetMapping("/")
 	public String home() {
-		return "Hello world by mukesh";
+		return "CICD Successfull";
 	}
 
 }
