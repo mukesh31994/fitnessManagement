43f78bbac545138f96379e6bc04f8a12f0f0f82a
unknown <mukeshsharma31994@gmail.com>
2021-11-17 19:25:49 +0000

    first commit

---
+++ src/main/java/com/java/azure/springboot/FitnessManagementApplication.java
@@ -0,0 +1,26 @@
+package com.java.azure.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FitnessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessManagementApplication.class, args);
+	}
+	
+	@GetMapping("welcome")
+	public String getMessage() {
+		return "Welcome to Azure Web App for Containers ";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "Hello world by mukesh";
+	}
+
+}
---
+++ src/test/java/com/java/azure/springboot/FitnessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.azure.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
